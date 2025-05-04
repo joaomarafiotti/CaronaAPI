550f93ef477282a669db8c415126982bc6198842
gcontiero11 <gustcontiero@gmail.com>
2025-05-04 18:28:19 -0300

    test: adding test to check if the CreateRideSolicitationUseCase throws a IllegalArgumentException when passing null args to it

--- src/test/java/br/ifsp/demo/usecase/CreateRideSolicitationUseCaseTest.java
+++ src/test/java/br/ifsp/demo/usecase/CreateRideSolicitationUseCaseTest.java
@@ -1,21 +1,25 @@
 package br.ifsp.demo.usecase;
 
 import br.ifsp.demo.domain.*;
-import br.ifsp.demo.repositories.RideRepository;
 import br.ifsp.demo.repositories.RideSolicitationRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 public class CreateRideSolicitationUseCaseTest {
@@ -67,5 +71,47 @@ public class CreateRideSolicitationUseCaseTest {
         RideSolicitation rideSolicitation = sut.createAndRegisterRideSolicitationFor(passenger, ride);
 
         assertThat(driver.getRideSolicitations()).isEqualTo(List.of(rideSolicitation));
+    }
+
+    @ParameterizedTest
+    @Tag("UnitTest")
+    @Tag("TDD")
+    @MethodSource("nullArgsSource")
+    @DisplayName("Should Throw IllegalArgumentException when receiving wrong args")
+    public void shouldThrowIllegalArgumentExceptionWhenReceivingWrongArgs(Passenger passenger, Ride ride) {
+        assertThrows(IllegalArgumentException.class, () -> sut.createAndRegisterRideSolicitationFor(passenger, ride));
+    }
+
+    private static Stream<Arguments> nullArgsSource() {
+        LocalDateTime now = LocalDateTime.now();
+        Driver driver = new Driver(
+                "Gustavo",
+                "123.456.789-X",
+                "motorista@gmail.com",
+                LocalDate.of(2004, 5, 6)
+        );
+        Car car = new Car(
+                "Fiat",
+                "Palio",
+                "Prata",
+                4,
+                "DQC1-ADQ"
+        );
+        Passenger passenger = new Passenger(
+                "Pedro",
+                "passageiro@gmail.com"
+        );
+        Ride ride = new Ride(
+                "Rua São João Bosco, 1324",
+                "Av. Miguel Petroni, 321",
+                now,
+                driver,
+                car
+        );
+
+        return Stream.of(
+                Arguments.of(null, ride),
+                Arguments.of(passenger, null)
+        );
     }
 }
