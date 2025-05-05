241b57731cf132f3e4cdaf4d26a19ac0507c6f38
brunomascioli <souzamasciolibruno@gmail.com>
2025-05-05 18:01:03 -0300

    feat: add validation for car request model fields: seats and licensePlate

--- src/main/java/br/ifsp/demo/models/request/CarRequestModel.java
+++ src/main/java/br/ifsp/demo/models/request/CarRequestModel.java
@@ -2,6 +2,8 @@ package br.ifsp.demo.models.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 
 import java.util.UUID;
 
@@ -9,7 +11,15 @@ public record CarRequestModel(
         @NotBlank String brand,
         @NotBlank String model,
         @NotBlank String color,
-        @NotNull Integer seats,
-        @NotBlank String licensePlate,
+
+        @NotNull
+        @Positive(message = "Seats must be a positive number")
+        Integer seats,
+        @NotBlank
+        @Pattern(
+                regexp = "^[A-Z]{3}[0-9][A-Z][0-9]{2}$|^[A-Z]{3}[0-9]{4}$",
+                message = "Invalid license plate format"
+        )
+        String licensePlate,
         @NotNull UUID driverId
 ) {}
