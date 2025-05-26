a1571305a7ea887f29807debf8301c18cd36e606
brunomascioli <souzamasciolibruno@gmail.com>
2025-05-26 19:02:52 -0300

    fix: refactor RideSolicitationController to use AuthenticationInfoService for passenger and driver ID

--- src/main/java/br/ifsp/demo/controller/rideSolicitation/RideSolicitationController.java
+++ src/main/java/br/ifsp/demo/controller/rideSolicitation/RideSolicitationController.java
@@ -7,6 +7,7 @@ import br.ifsp.demo.domain.RideSolicitation;
 import br.ifsp.demo.repositories.DriverRepository;
 import br.ifsp.demo.repositories.PassengerRepository;
 import br.ifsp.demo.repositories.RideRepository;
+import br.ifsp.demo.security.auth.AuthenticationInfoService;
 import br.ifsp.demo.usecase.ride_solicitation.CreateRideSolicitationUseCase;
 import br.ifsp.demo.usecase.ride_solicitation.GetRideSolicitationUseCase;
 import br.ifsp.demo.usecase.ride_solicitation.ManageRideSolicitationUseCase;
@@ -29,10 +30,12 @@ public class RideSolicitationController {
     private final PassengerRepository passengerRepository;
     private final RideRepository rideRepository;
     private final DriverRepository driverRepository;
+    private final AuthenticationInfoService authenticationInfoService;
 
     @PostMapping
-    public ResponseEntity<RideSolicitation> createSolicitation(@RequestParam UUID passengerId,
-                                                               @RequestParam UUID rideId) {
+    public ResponseEntity<RideSolicitation> createSolicitation(@RequestParam UUID rideId) {
+        UUID passengerId = authenticationInfoService.getAuthenticatedUserId();
+
         Passenger passenger = passengerRepository.findById(passengerId).orElseThrow(() -> new EntityNotFoundException("Passenger with id:" + passengerId + " not found"));
         Ride ride = rideRepository.findById(rideId).orElseThrow(() -> new EntityNotFoundException("Ride with id:" + rideId + " not found"));
 
@@ -41,25 +44,26 @@ public class RideSolicitationController {
     }
 
     @GetMapping("/pending")
-    public ResponseEntity<List<RideSolicitation>> getPendingSolicitations(@RequestParam UUID driverId) {
+    public ResponseEntity<List<RideSolicitation>> getPendingSolicitations() {
+        UUID driverId = authenticationInfoService.getAuthenticatedUserId();
         Driver driver = driverRepository.findById(driverId).orElseThrow(() -> new EntityNotFoundException("Driver with id:" + driverId + " not found"));
 
         List<RideSolicitation> pendingSolicitations = getRideSolicitationUseCase.getPendingSolicitationsFrom(driver);
         return ResponseEntity.ok(pendingSolicitations);
     }
 
-    @PostMapping("/{solicitationId}/accept")
-    public ResponseEntity<RideSolicitation> acceptSolicitation(@PathVariable UUID solicitationId,
-                                                               @RequestParam UUID driverId) {
+    @PostMapping("/accept")
+    public ResponseEntity<RideSolicitation> acceptSolicitation(@PathVariable UUID solicitationId) {
+        UUID driverId = authenticationInfoService.getAuthenticatedUserId();
         Driver driver = driverRepository.findById(driverId).orElseThrow(() -> new EntityNotFoundException("Driver with id:" + driverId + " not found"));
 
         RideSolicitation accepted = manageRideSolicitationUseCase.acceptSolicitationFor(solicitationId, driver);
         return ResponseEntity.ok(accepted);
     }
 
-    @PostMapping("/{solicitationId}/reject")
-    public ResponseEntity<RideSolicitation> rejectSolicitation(@PathVariable UUID solicitationId,
-                                                               @RequestParam UUID driverId) {
+    @PostMapping("/reject")
+    public ResponseEntity<RideSolicitation> rejectSolicitation(@PathVariable UUID solicitationId) {
+        UUID driverId = authenticationInfoService.getAuthenticatedUserId();
         Driver driver = driverRepository.findById(driverId).orElseThrow(() -> new EntityNotFoundException("Driver with id:" + driverId + " not found"));
 
         RideSolicitation rejected = manageRideSolicitationUseCase.rejectSolicitationFor(solicitationId, driver);
