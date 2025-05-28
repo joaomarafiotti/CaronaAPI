9db214a9ee174c05fd3638d2c43c3922cb83628d
gcontiero11 <gustcontiero@gmail.com>
2025-05-28 18:40:41 -0300

    refactor: adding an example at the rideRequestModel

--- backend/src/main/java/br/ifsp/demo/models/request/RideRequestModel.java
+++ backend/src/main/java/br/ifsp/demo/models/request/RideRequestModel.java
@@ -1,5 +1,6 @@
 package br.ifsp.demo.models.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -7,9 +8,13 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record RideRequestModel(
-        @NotBlank String startAddress,
-        @NotBlank String endAddress,
-        @NotNull LocalDateTime departureTime,
-        @NotNull UUID carId
+        @NotBlank @Schema(example = "Rua das Palmeiras, 250B, Jardim América, São Paulo")
+        String startAddress,
+        @NotBlank @Schema(example = "Av. Brasil, 1020, Centro, Rio de Janeiro")
+        String endAddress,
+        @NotNull @Schema(example = "2025-05-28T14:30:00")
+        LocalDateTime departureTime,
+        @NotNull
+        UUID carId
 ) {
 }
