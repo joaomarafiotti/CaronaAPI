daf32251891860ea1b61f55326e7397280a7ac67
brunomascioli <souzamasciolibruno@gmail.com>
2025-05-29 09:47:23 -0300

    feat: add ProtectedDriverRoute component for route protection based on user role

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import RegisterDriverPage from './pages/RegisterDriverPage';
 import './App.css';
 import { AuthProvider } from './context/AuthContext';
 import { ProtectedPassengerRoute } from './components/ProtectedPassengerRoute';
+import { ProtectedDriverRoute } from './components/ProtectedDriverRoute';
 import Dashboard from './pages/Dashboard';
 
 function App() {
@@ -16,11 +17,19 @@ function App() {
         <Route path="/register-passenger" element={<RegisterPassengerPage />} />
         <Route path="/register-driver" element={<RegisterDriverPage />} />
         <Route
-          path="/dashboard"
+          path="/passenger-dashboard"
           element={
             <ProtectedPassengerRoute>
               <Dashboard />
             </ProtectedPassengerRoute>
+          }
+        />
+        <Route
+          path="/driver-dashboard"
+          element={
+            <ProtectedDriverRoute>
+              <Dashboard />
+            </ProtectedDriverRoute>
           }
         />
         <Route path="*" element={<Navigate to="/login" replace={true} />} />
