aa69337aad53a9177e297d4adda0b06f4afece73
gcontiero11 <gustcontiero@gmail.com>
2025-05-29 15:53:33 -0300

    feat: starting to implement Passenger Header

--- frontend/src/pages/dashboard/PassengerDashBoard.jsx
+++ frontend/src/pages/dashboard/PassengerDashBoard.jsx
@@ -1,10 +1,15 @@
-import { useAuth } from '../../context/AuthContext';
-import { useNavigate } from 'react-router-dom';
+import { useAuth } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const PassengerDashBoard = () => {
-    return (
-        <div className="dashboard-container"/>
-    );
+  return (
+    <>
+      <p style={{ fontSize: "24px", fontWeight: "bold" }}>
+        Welcome to the Driver Dashboard! Here you can manage your rides, view
+        your earnings, and update your profile.
+      </p>
+    </>
+  );
 };
 
 export default PassengerDashBoard;
--- frontend/src/pages/dashboard/layout/PassengerHeader.jsx
+++ frontend/src/pages/dashboard/layout/PassengerHeader.jsx
@@ -6,13 +6,33 @@ export const PassengerHeader = () => {
 
   return (
     <header className="header">
-      <h1 className="header-title">Carona APP</h1>
-      <nav>
+      <div className="header-left">
+        <h1 className="header-title">Carona APP</h1>
         <button
           className="header-btn"
           onClick={() => navigate("/dashboard/passenger")}
         >
           Passageiro
+        </button>
+        <button
+          className="header-btn"
+          onClick={() => navigate("/dashboard/passenger/rides")}
+        >
+          Visualizar caronas disponíveis
+        </button>
+        <button
+          className="header-btn"
+          onClick={() => navigate("/dashboard/passenger/history")}
+        >
+          Visualizar histórico de carona
+        </button>
+      </div>
+      <nav>
+        <button
+          className="header-btn header-btn-profile"
+          onClick={() => navigate("/dashboard/profile")}
+        >
+          Perfil
         </button>
       </nav>
     </header>
