d14befa4b9a6d2e34857c698aae25c607c551604
gcontiero11 <gustcontiero@gmail.com>
2025-06-02 20:03:48 -0300

    fix: fixing JPAQuery

--- backend/src/main/java/br/ifsp/demo/repositories/CarRepository.java
+++ backend/src/main/java/br/ifsp/demo/repositories/CarRepository.java
@@ -1,11 +1,12 @@
 package br.ifsp.demo.repositories;
 
 import br.ifsp.demo.domain.Car;
+import br.ifsp.demo.domain.LicensePlate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CarRepository extends JpaRepository<Car, UUID> {
-    Optional<Car> findByLicensePlate(String licensePlate);
+    Optional<Car> findByLicensePlate(LicensePlate licensePlate);
 }
