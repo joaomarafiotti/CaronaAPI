504784c62bfa6de383ae56c70b69921ca7c8a3ac
gcontiero11 <gustcontiero@gmail.com>
2025-06-02 20:10:38 -0300

    fix: adding filters to availableOnes method into GetRideUseCase

--- backend/src/main/java/br/ifsp/demo/usecase/ride/GetRideUseCase.java
+++ backend/src/main/java/br/ifsp/demo/usecase/ride/GetRideUseCase.java
@@ -2,9 +2,12 @@ package br.ifsp.demo.usecase.ride;
 
 import br.ifsp.demo.domain.Driver;
 import br.ifsp.demo.domain.Ride;
+import br.ifsp.demo.domain.RideSolicitation;
 import br.ifsp.demo.models.response.RideResponseModel;
 import br.ifsp.demo.repositories.PassengerRepository;
 import br.ifsp.demo.repositories.RideRepository;
+import br.ifsp.demo.repositories.RideSolicitationRepository;
+import br.ifsp.demo.utils.RideSolicitationStatus;
 import br.ifsp.demo.utils.RideStatus;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,9 +19,12 @@ import java.util.UUID;
 @AllArgsConstructor
 public class GetRideUseCase {
     private final RideRepository rideRepository;
+    private final RideSolicitationRepository rideSolicitationRepository;
 
     public List<RideResponseModel> availableOnes(UUID passengerId) {
         List<Ride> rides = rideRepository.findAll();
+        List<RideSolicitation> passengerSolicitations =
+                rideSolicitationRepository.findRideSolicitationsByPassenger_Id(passengerId);
 
 
         if (rides.isEmpty()) return List.of();
@@ -26,6 +32,10 @@ public class GetRideUseCase {
         return rides.stream()
                 .filter(r -> r.getRideStatus().equals(RideStatus.WAITING) || r.getRideStatus().equals(RideStatus.FULL))
                 .filter(r -> r.getPassengers().stream().noneMatch(p -> p.getId().equals(passengerId)))
+                .filter(r -> passengerSolicitations
+                        .stream()
+                        .filter(s -> s.getStatus().equals(RideSolicitationStatus.WAITING))
+                        .noneMatch(s -> s.getRide().getId().equals(r.getId())))
                 .map(Ride::toResponseModel)
                 .toList();
     }
