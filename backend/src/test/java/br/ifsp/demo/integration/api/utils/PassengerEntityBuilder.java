3d95bad5c06dbce9c7052cb76523e41f32835fee
akira-erick <kajitani.motta@gmail.com>
2025-06-16 10:41:38 -0300

    test(api-auth): getting random cpfs

--- backend/src/test/java/br/ifsp/demo/integration/api/utils/PassengerEntityBuilder.java
+++ backend/src/test/java/br/ifsp/demo/integration/api/utils/PassengerEntityBuilder.java
@@ -6,12 +6,19 @@ import br.ifsp.demo.security.user.User;
 import com.github.javafaker.Faker;
 
 import java.time.LocalDate;
-import java.util.Locale;
-import java.util.UUID;
+import java.util.*;
 
 public class PassengerEntityBuilder {
 
     private static Faker faker = new Faker(new Locale("pt-BR"));
+    private static List<String> cpfs = List.of(
+            "679.538.950-21",
+            "733.677.500-42",
+            "804.530.890-70",
+            "241.811.950-58",
+            "877.321.140-00",
+            "781.669.170-09"
+    );
 
     public static User createRandomPassengerUser(String password){
         return User.builder().id(UUID.randomUUID())
@@ -23,6 +30,11 @@ public class PassengerEntityBuilder {
                 .cpf(Cpf.of("679.538.950-21"))
                 .birthDate(LocalDate.parse(faker.date().birthday(20, 80).toString()))
                 .build();
+    }
+
+    private static String getRandomCPF(){
+        Random rand = new Random();
+        return cpfs.get(rand.nextInt(cpfs.size()));
     }
 
 }
