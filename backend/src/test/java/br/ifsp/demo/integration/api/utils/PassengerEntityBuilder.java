9cb11f306c70b601d41a11a7d495d09a5377f27e
akira-erick <kajitani.motta@gmail.com>
2025-06-16 10:30:20 -0300

    test(api-auth): Implementing utils random passanger user

--- backend/src/test/java/br/ifsp/demo/integration/api/utils/PassengerEntityBuilder.java
+++ backend/src/test/java/br/ifsp/demo/integration/api/utils/PassengerEntityBuilder.java
@@ -1,4 +1,28 @@
 package br.ifsp.demo.integration.api.utils;
 
+import br.ifsp.demo.domain.Cpf;
+import br.ifsp.demo.security.user.Role;
+import br.ifsp.demo.security.user.User;
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.util.Locale;
+import java.util.UUID;
+
 public class PassengerEntityBuilder {
+
+    private static Faker faker = new Faker(new Locale("pt-BR"));
+
+    public static User createRandomPassengerUser(String password){
+        return User.builder().id(UUID.randomUUID())
+                .name(faker.name().firstName())
+                .lastname(faker.name().lastName())
+                .email(faker.internet().emailAddress())
+                .password(password)
+                .role(Role.PASSENGER)
+                .cpf(Cpf.of("679.538.950-21"))
+                .birthDate(LocalDate.parse(faker.date().birthday(20, 80).toString()))
+                .build();
+    }
+
 }
