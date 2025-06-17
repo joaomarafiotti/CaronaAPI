6ca179fd5f36ec3a6dcef45811d6ced10414ebd3
antonioacampos <campos.antonio@aluno.ifsp.edu.br>
2025-06-17 10:16:46 -0300

    feat(test): add TestConfig with embedded H2 datasource for testing

---
+++ backend/src/test/java/br/ifsp/demo/config/TestConfig.java
@@ -0,0 +1,21 @@
+package br.ifsp.demo.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.sql.DataSource;
+
+@TestConfiguration
+@ActiveProfiles("test")
+public class TestConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .build();
+    }
+} 
