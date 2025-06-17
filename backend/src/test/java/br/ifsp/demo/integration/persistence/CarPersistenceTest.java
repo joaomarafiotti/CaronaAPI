c8aa7076948a293c76a3f23e1a6a403b1b66147d
antonioacampos <campos.antonio@aluno.ifsp.edu.br>
2025-06-17 10:22:36 -0300

    feat(test): create BeforeEach method to setup drivers

--- backend/src/test/java/br/ifsp/demo/integration/persistence/CarPersistenceTest.java
+++ backend/src/test/java/br/ifsp/demo/integration/persistence/CarPersistenceTest.java
@@ -1,7 +1,11 @@
 package br.ifsp.demo.integration.persistence;
 
+import br.ifsp.demo.domain.Driver;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -10,4 +14,18 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 @DisplayName("Car Persistence Tests")
 class CarPersistenceTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    private Driver driver;
+    private Driver anotherDriver;
+
+    @BeforeEach
+    void setUp() {
+        entityManager.clear();
+
+        driver = createDriver("Fulano", "Silva", "fulano.silva@gmail.com", "005.046.860-03");
+        anotherDriver = createDriver("Ciclano", "Souza", "ciclano.souza@outlook.com", "355.553.060-75");
+    }
 }
