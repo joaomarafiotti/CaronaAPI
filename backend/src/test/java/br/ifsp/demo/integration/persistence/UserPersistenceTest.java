3a78f8dd76a482eb4d24c269a58415e351de0f31
antonioacampos <campos.antonio@aluno.ifsp.edu.br>
2025-06-18 12:16:13 -0300

    feat(test): create UserPersistenceTest class

---
+++ backend/src/test/java/br/ifsp/demo/integration/persistence/UserPersistenceTest.java
@@ -0,0 +1,13 @@
+package br.ifsp.demo.integration.persistence;
+
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@Import(br.ifsp.demo.config.TestConfig.class)
+@ActiveProfiles("test")
+@DisplayName("User Persistence Tests")
+class UserPersistenceTest {
+}
