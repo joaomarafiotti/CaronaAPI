b7480373e0604f63db595d69bce30de3d4905a97
antonioacampos <campos.antonio@aluno.ifsp.edu.br>
2025-06-20 15:24:36 -0300

    feat(test): create auxiliator method to assert driver properties

--- backend/src/test/java/br/ifsp/demo/integration/persistence/UserPersistenceTest.java
+++ backend/src/test/java/br/ifsp/demo/integration/persistence/UserPersistenceTest.java
@@ -3,11 +3,14 @@ package br.ifsp.demo.integration.persistence;
 import br.ifsp.demo.domain.Cpf;
 import br.ifsp.demo.domain.Driver;
 import br.ifsp.demo.domain.Passenger;
+import br.ifsp.demo.security.user.Role;
 import org.junit.jupiter.api.DisplayName;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
 @Import(br.ifsp.demo.config.TestConfig.class)
@@ -22,5 +25,13 @@ class UserPersistenceTest {
     private Passenger createPassenger(String firstName, String lastName, String email, String cpf) {
         return new Passenger(firstName, lastName, email, "password123",
                 Cpf.of(cpf), LocalDate.of(1992, 2, 2));
+    }
+
+    private void assertDriverProperties(Driver driver, String name, String lastname, String email, String cpf) {
+        assertThat(driver.getName()).isEqualTo(name);
+        assertThat(driver.getLastname()).isEqualTo(lastname);
+        assertThat(driver.getEmail()).isEqualTo(email);
+        assertThat(driver.getCpf().toString()).isEqualTo(cpf);
+        assertThat(driver.getRole()).isEqualTo(Role.DRIVER);
     }
 }
