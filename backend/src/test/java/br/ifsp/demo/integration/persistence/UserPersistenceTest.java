8b2c67771967f786e9bf14fcafba7dbda4c1f0c6
antonioacampos <campos.antonio@aluno.ifsp.edu.br>
2025-06-20 15:22:48 -0300

    feat(test): create auxiliator method createPassenger

--- backend/src/test/java/br/ifsp/demo/integration/persistence/UserPersistenceTest.java
+++ backend/src/test/java/br/ifsp/demo/integration/persistence/UserPersistenceTest.java
@@ -2,6 +2,7 @@ package br.ifsp.demo.integration.persistence;
 
 import br.ifsp.demo.domain.Cpf;
 import br.ifsp.demo.domain.Driver;
+import br.ifsp.demo.domain.Passenger;
 import org.junit.jupiter.api.DisplayName;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
@@ -16,5 +17,10 @@ class UserPersistenceTest {
     private Driver createDriver(String firstName, String lastName, String email, String cpf) {
         return new Driver(firstName, lastName, email, "password123",
                 Cpf.of(cpf), LocalDate.of(1990, 1, 1));
+    }
+
+    private Passenger createPassenger(String firstName, String lastName, String email, String cpf) {
+        return new Passenger(firstName, lastName, email, "password123",
+                Cpf.of(cpf), LocalDate.of(1992, 2, 2));
     }
 }
