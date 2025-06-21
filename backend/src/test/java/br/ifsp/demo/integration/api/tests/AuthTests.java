914271d6c88f983252560d7f835db11fe7c47303
akira-erick <kajitani.motta@gmail.com>
2025-06-21 18:19:39 -0300

    test(api-authentication): test to verify registration of passenger and payload

--- backend/src/test/java/br/ifsp/demo/integration/api/tests/AuthTests.java
+++ backend/src/test/java/br/ifsp/demo/integration/api/tests/AuthTests.java
@@ -1,4 +1,23 @@
 package br.ifsp.demo.integration.api.tests;
 
-public class AuthTests {
+import br.ifsp.demo.integration.api.utils.PassengerEntityBuilder;
+import br.ifsp.demo.security.user.User;
+import io.restassured.filter.log.LogDetail;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class AuthTests extends BaseApiIntegrationTest {
+
+    @Test
+    @DisplayName("Should register passenger and return 201 with id as payload")
+    void shouldRegisterUserAndReturn201WithIdAsPayload(){
+        final User user = PassengerEntityBuilder.createRandomPassengerUser("password");
+        given().contentType("application/json").port(port).body(user)
+                .when().post(("/api/v1/register"))
+                .then().log().ifValidationFails(LogDetail.BODY).statusCode(201).body("id", notNullValue());
+    }
 }
---
+++ backend/src/test/java/br/ifsp/demo/integration/api/tests/BaseApiIntegrationTest.java
@@ -0,0 +1,29 @@
+package br.ifsp.demo.integration.api.tests;
+
+
+import br.ifsp.demo.security.user.JpaUserRepository;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BaseApiIntegrationTest {
+
+    @LocalServerPort protected int port = 8080;
+    @Autowired private JpaUserRepository repository;
+
+    @BeforeEach
+    public void generalSetup() {
+        RestAssured.baseURI = "http://localhost:8080";
+    }
+
+    @AfterEach void tearDown() {
+        repository.deleteAll();
+    }
+
+}
